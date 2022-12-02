343458771e4a123e0fbcc717166672b4604279e1
Alexander <alexander.parke2002@gmail.com>
2022-12-02 23:45:58 +0400

    Alexander

--- JavaScript/game.js
+++ JavaScript/game.js
@@ -2,6 +2,7 @@ window.onload = function () {
     // let btn_click = document.getElementById("click");
     // btn_click.onclick = update();
 }
+var rndNum;
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 
@@ -24,8 +25,14 @@ var canvas2 = document.getElementById("myCanvas2");
 var context2 = canvas2.getContext("2d");
 var context3 = canvas2.getContext("2d");
 
-var rndNum = Math.floor((Math.random() * 6) + 1);
-document.getElementById("click").innerHTML = rndNum;
+// var rndNum = Math.floor((Math.random() * 6) + 1);
+// document.getElementById("click").innerHTML = rndNum;
+
+function random_generator() {
+    rndNum = 1;
+    // rndNum = Math.floor((Math.random() * 6) + 1);
+    // document.getElementById("click").innerHTML = rndNum;
+}
 
 let dx = -58;
 let dy = -58;
@@ -38,7 +45,10 @@ let y2 = 630;
 const myInterval = setInterval(draw, 1000);
 
 function draw() {
+    random_generator()
     context2.clearRect(0, 0, canvas2.width, canvas2.height);
+    context3.clearRect(0, 0, canvas2.width, canvas2.height);
+
     context2.beginPath()
     context2.arc(x2, y2, 13, 0, 2 * Math.PI);
     context2.fillStyle = "blue";
@@ -57,7 +67,7 @@ function draw() {
             continue;
         } 
         if (x < 600 && x > 150 && y == 660) {
-            x += dx;
+            x += -58;
             continue;
         }
         if (x > 100 && x < 150 && y == 660) {
@@ -69,7 +79,7 @@ function draw() {
             continue;
         } 
         if (y < 600 && y > 150 && x < 100) {
-            y += dy;
+            y += -58;
             continue;
         }
         if (y > 100 && y < 150 && x < 100) {
@@ -98,6 +108,58 @@ function draw() {
         } 
         if (y > 550 && y < 600 && x == 650) {
             y += 72;
+            continue;
+        } 
+    }
+
+    for (let num = 0; num < rndNum; num++) {
+        if (x2 == 650 && y2 == 630) {
+            x2 += -72;
+            continue;
+        } 
+        if (x2 < 600 && x2 > 150 && y2 == 630) {
+            x2 += -58;
+            continue;
+        }
+        if (x2 > 100 && x2 < 150 && y2 == 630) {
+            x2 += -72;
+            continue;
+        } 
+        if (x2 < 100 && y2 == 630) {
+            y2 += -72;
+            continue;
+        } 
+        if (y2 < 600 && y2 > 100 && x2 < 100) {
+            y2 += -58;
+            continue;
+        }
+        if (y2 < 100 && y2 >80 && x2 < 100) {
+            y2 += -72;
+            continue;
+        } 
+        if (x2 < 100 && y2 < 100) {
+            x2 += 72;
+            continue;
+        } 
+        if (x2 < 550 && x2 > 100 && y2 < 100) {
+            x2 += 58;
+            continue;
+        }
+        if (x2 > 550 && x2 < 600 && y2 < 100) {
+            x2 += 72;
+            continue;
+        } 
+        if (x2 == 650 && y2 < 80) {
+            y2 += 72;
+            continue;
+        } 
+        if (y2 < 520 && y2 > 80 && x2 == 650) {
+            y2 += 58;
+            continue;
+        } 
+        if (y2 > 520 && y2 < 600 && x2 == 650) {
+            y2 += 72;
+            continue;
         } 
     }
     clearInterval(myInterval);
